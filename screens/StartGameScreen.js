edeb32f5959aa7103e9233afdf90ba297a432b3b
John Smith <johnsmithcoder1@gmail.com>
2021-10-21 14:18:15 -0500

    Handle NaN input confirmation

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -21,7 +21,7 @@ const StartGameScreen = props => {
 
   const confirmInputHandler = () => {
     const chosenNumber = parseInt(enteredValue);
-    if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
       Alert.alert(
         'Invalid Input', 
         'Please enter a number between 1 and 99.', 
