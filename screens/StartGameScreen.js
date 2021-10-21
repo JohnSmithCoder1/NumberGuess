f8f48a417e1ce393c62c1b15d9ef09f47b13462d
John Smith <johnsmithcoder1@gmail.com>
2021-10-21 15:30:26 -0500

    Add and implement NumberContainer and styling to show confirmed input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,6 +4,7 @@ import { Alert, Button, Keyboard, StyleSheet, Text, TouchableWithoutFeedback, Vi
 import Card from '../components/Card';
 import Colors from '../constants/colors';
 import Input from '../components/Input';
+import NumberContainer from '../components/NumberContainer';
 
 const StartGameScreen = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -32,12 +33,19 @@ const StartGameScreen = props => {
     setConfirmed(true);
     setSelectedNumber(chosenNumber);
     setEnteredValue('');
+    Keyboard.dismiss;
   };
 
   let confirmedOutput;
 
   if (confirmed) {
-    confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>
+    confirmedOutput = (
+      <Card style={styles.summaryContainer}>
+        <Text>You selected</Text>
+        <NumberContainer>{selectedNumber}</NumberContainer>
+        <Button title='START NEW GAME' />
+      </Card>
+    );
   }
 
   return (
@@ -103,6 +111,10 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 10,
+    alignItems: 'center',
+  },
+  summaryContainer: {
+    marginTop: 20,
     alignItems: 'center',
   },
   title: {
