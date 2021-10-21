b0fea370ba9a7c8e76646b7b8dad0a8737ec42c4
John Smith <johnsmithcoder1@gmail.com>
2021-10-21 15:59:01 -0500

    Add and implement GameScreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -43,7 +43,7 @@ const StartGameScreen = props => {
       <Card style={styles.summaryContainer}>
         <Text>You selected</Text>
         <NumberContainer>{selectedNumber}</NumberContainer>
-        <Button title='START NEW GAME' />
+        <Button title='START NEW GAME' onPress={() => props.onStartGame(selectedNumber)}/>
       </Card>
     );
   }
