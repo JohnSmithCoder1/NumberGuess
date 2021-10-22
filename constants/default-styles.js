3aa49171a6e0cfa91673c052f002d612fb50a78c
John Smith <johnsmithcoder1@gmail.com>
2021-10-22 12:43:31 -0500

    Add title to default styles

--- constants/default-styles.js
+++ constants/default-styles.js
@@ -3,5 +3,9 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   bodyText: {
     fontFamily: 'open-sans',
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 16,
   }
 });
