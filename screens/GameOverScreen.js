d21e0bfbe29d53047952d8866ca429ef15b906a3
John Smith <johnsmithcoder1@gmail.com>
2021-10-22 12:43:47 -0500

    Add image to game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 
 import BodyText from '../components/BodyText';
+import DefaultStyles from '../constants/default-styles';
 
 const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
-      <BodyText>Aha! I got it!</BodyText>
+      <Text style={DefaultStyles.title}>Aha! I got it!</Text>
+      <View style={styles.imageContainer}>
+        <Image 
+          style={styles.image} 
+          source={require('../assets/success.png')} 
+          // cover is the default value, but leaving this here to show how to change it
+          resizeMode='cover'
+        />
+      </View>
       <BodyText>Your number is {props.userNumber}.</BodyText>
       <BodyText>It only took me {props.numberOfRounds} guesses!</BodyText>
       <View style={styles.buttonContainer} >
@@ -18,7 +27,21 @@ const GameOverScreen = props => {
 
 const styles = StyleSheet.create({
   buttonContainer: {
-    marginTop: 20,
+    marginTop: 10,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },  
+  imageContainer: {
+    width: 300,
+    height: 300,
+    // for a perfect circle, borderRadius should be half the width and height
+    borderRadius: 150,
+    borderWidth: 2,
+    borderColor: 'black',
+    overflow: 'hidden',
+    marginVertical: 30,
   },
   screen: {
     flex: 1,
