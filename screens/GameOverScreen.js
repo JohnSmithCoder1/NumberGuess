9fd7853d5f4ca134e2c21dcf49cbb505ba7ff885
John Smith <johnsmithcoder1@gmail.com>
2021-10-21 20:24:38 -0500

    Add styling and try again button to game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 const GameOverScreen = props => {
   return (
-    <View>
-      <Text>Aha! I got it.</Text>
+    <View style={styles.screen}>
+      <Text>Aha! I got it!</Text>
+      <Text>Your number is {props.userNumber}.</Text>
+      <Text>It only took me {props.numberOfRounds} guesses!</Text>
+      <View style={styles.buttonContainer} >
+        <Button title="Try Again?" onPress={props.onRestart} />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  
+  buttonContainer: {
+    marginTop: 20,
+  },
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 
 export default GameOverScreen;
